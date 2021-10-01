540167548a7be11cc915d285389381d89304473d
ysur67 <rus.muratow@gmail.com>
2021-10-02 00:42:55 +0300

    feat: override update method

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,4 +1,3 @@
-from celery.utils.imports import instantiate
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 from rest_framework import viewsets
@@ -37,6 +36,28 @@ class TaskViewSet(viewsets.ModelViewSet):
         current_task_id = self.get_int_value(response.data, "id")
         run_scan_task.delay(current_task_id)
         return response
+
+    def update(self, request: Request, *args, **kwargs):
+        current_task: Task = self.get_object()
+        response = {
+            "message": str()
+        }
+        BAD_REQUEST_CODE = 422
+        if current_task.is_running:
+            response["message"] = ("At the moment task "
+                                   "is running and cannot be changed")
+            return Response(response, status=BAD_REQUEST_CODE)
+        request_data = request.data.copy()
+        new_ip_range = request_data.get("ip_range", None)
+        if new_ip_range and current_task.is_finished:
+            response["message"] = ("Task has already been finished "
+                                   "and its ip range cannot be changed")
+            return Response(response, BAD_REQUEST_CODE)
+        new_name = request_data.get("name", None)
+        current_task.set_name(new_name)
+        current_task.set_ip_range(new_ip_range)
+        response["message"] = "The task has been successfully updated"
+        return Response(response)
 
     def get_int_value(self, dict_, key):
         DEFAULT_VALUE = 0
