ed9d0a1212dcdb61be9cdfbe61adb11cbd1020c6
ysur67 <rus.muratow@gmail.com>
2021-10-01 23:44:14 +0300

    feat: create method for tasks' viewset

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,8 +1,10 @@
+from celery.utils.imports import instantiate
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
+from tasks.tasks import run_scan_task
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -21,6 +23,20 @@ class TaskViewSet(viewsets.ModelViewSet):
         if range_size != 0:
             qs = qs[:range_size]
         return Response(self.get_serializer(qs, many=True).data)
+
+    def create(self, request, *args, **kwargs):
+        # Создать объект, как обычно и только потом запускаем задачу
+        response = super().create(request, *args, **kwargs)
+        if response.status_code != 201:
+            return response
+        autostart = self.request.data.get("autostart", False)
+        if not autostart:
+            return response
+        if not isinstance(autostart, bool):
+            return response
+        current_task_id = self.get_int_value(response.data, "id")
+        run_scan_task.delay(current_task_id)
+        return response
 
     def get_int_value(self, dict_, key):
         DEFAULT_VALUE = 0
