e5a53bb477b812627ec872726ffeb2572e8e2fc3
ysur67 <rus.muratow@gmail.com>
2021-10-01 21:36:10 +0300

    fix: set result field as json field in tasks' serializer

--- project/tasks/serializers.py
+++ project/tasks/serializers.py
@@ -3,6 +3,8 @@ from tasks.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    result = serializers.JSONField()
+
     class Meta:
         model = Task
         fields = "__all__"
