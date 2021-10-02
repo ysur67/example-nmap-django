f1dad33930ee6129c80ac0ece838099d1eba9076
ysur67 <rus.muratow@gmail.com>
2021-10-03 01:43:57 +0300

    feat: redirect from base url to swagger

--- project/project/urls.py
+++ project/project/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.views.generic import RedirectView
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -16,6 +18,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+   path('', RedirectView.as_view(url='/swagger/')),
    path("swagger/", schema_view.with_ui("swagger"), name="schema-swagger-ui"),
    path('admin/', admin.site.urls),
    path('api/v1/', include(("tasks.urls", "tasks"))),
